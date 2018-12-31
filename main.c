481953f3cc2a65f850a9692ef2840a36e6a98e50
valmit <sliper98@gmail.com>
2018-12-31 11:50:30 +0700

    seems goode

--- main.c
+++ main.c
@@ -7,16 +7,27 @@
 
 int main(int argc, char** argv)
 {
+    if (argc < 2) {
+        printf("need arguments\n");
+        exit(EXIT_FAILURE);
+    }
     pid_t pid = fork();
+
     if (pid < 0) {
         perror("");
         exit(EXIT_FAILURE);
     }
     else if (pid == 0) { // we are child
-
+        if (execvp(argv[1], argv + 1) == -1) {
+            perror("exec");
+        }
     }
     else { // we are parent
-
+        int status;
+        wait(&status);
+        if (WIFEXITED(status)) {
+            printf("child process exited with status = %d", WEXITSTATUS(status));
+        }
     }
     return 0;
 }
